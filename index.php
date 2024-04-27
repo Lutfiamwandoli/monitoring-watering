5d0aafd991e1c0900d018e96ef13fa6387413611
Lutfiamwandoli <lutfiamwandoli@gmail.com>
2024-04-28 03:05:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,160 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- css bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md bg-danger navbar-dark">
+
+      <a class="navbar-brand" href="">ESP8266 & SENSOR RESISTIVE KELEMBABAN TANAH</a>
+    </div>
+  </div>
+</nav>
+  
+<div class="container">
+  <div align="center">
+    <h3><marquee><font color = "#DB7093"<b>DATA SENSOR KELEMBABAN TANAH</b></marquee></h3>
+  </div>
+  
+  <!-- Tabel data -->
+  <table class="table table-striped table-bordered table-hover">
+    <thead> 
+      <tr bgcolor="#FFA800" >
+        <th>No.</th>
+        <th>Data</th>
+        <th>Waktu</th>
+      </tr>
+    </thead>  
+    <tbody>
+      <?php
+      include "koneksi.php";
+     
+      $page = (isset($_GET['page']))? (int) $_GET['page'] : 1;
+      
+      $kolomCari=(isset($_GET['Kolom']))? $_GET['Kolom'] : "";
+     
+      $kolomKataKunci=(isset($_GET['KataKunci']))? $_GET['KataKunci'] : "";
+
+      // Jumlah data per halaman
+      $limit = 10;
+
+      $limitStart = ($page - 1) * $limit;
+      
+      //kondisi jika parameter pencarian kosong
+      if($kolomCari=="" && $kolomKataKunci==""){
+        $SqlQuery = mysqli_query($koneksi, "SELECT * FROM datasensor LIMIT ".$limitStart.",".$limit);
+      }else{
+        //kondisi jika parameter kolom pencarian diisi
+        $SqlQuery = mysqli_query($koneksi, "SELECT * FROM datasensor WHERE $kolomCari LIKE '%$kolomKataKunci%' LIMIT ".$limitStart.",".$limit);
+      }
+      
+      $no = $limitStart + 1;
+      
+      while($row = mysqli_fetch_array($SqlQuery)){ 
+      ?>
+        <tr>
+          <td><?php echo $no++; ?></td>
+          <td><?php echo $row['data']; ?></td>
+          <td><?php echo $row['waktu']; ?></td>
+        </tr>
+      <?php           
+      }
+      ?>
+    </tbody>      
+  </table>
+  <div align="right">
+    <ul class="pagination">
+      <?php
+        // Jika page = 1, maka LinkPrev disable
+        if($page == 1){ 
+      ?>        
+        <!-- link Previous Page disable --> 
+        <li class="disabled"><a href="#">Previous</a></li>
+      <?php
+        }
+        else{ 
+          $LinkPrev = ($page > 1)? $page - 1 : 1;  
+
+          if($kolomCari=="" && $kolomKataKunci==""){
+          ?>
+            <li><a href="index.php?page=<?php echo $LinkPrev; ?>">Previous</a></li>
+       <?php     
+          }else{
+        ?> 
+          <li><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $LinkPrev;?>">Previous</a></li>
+         <?php
+           } 
+        }
+      ?>
+
+      <?php
+        //kondisi jika parameter pencarian kosong
+        if($kolomCari=="" && $kolomKataKunci==""){
+          $SqlQuery = mysqli_query($koneksi, "SELECT * FROM datasensor");
+        }else{
+          //kondisi jika parameter kolom pencarian diisi
+          $SqlQuery = mysqli_query($koneksi, "SELECT * FROM datasensor WHERE $kolomCari LIKE '%$kolomKataKunci%'");
+        }     
+      
+        //Hitung semua jumlah data yang berada pada tabel Sisawa
+        $JumlahData = mysqli_num_rows($SqlQuery);
+        
+        // Hitung jumlah halaman yang tersedia
+        $jumlahPage = ceil($JumlahData / $limit); 
+        
+        // Jumlah link number 
+        $jumlahNumber = 1; 
+
+        // Untuk awal link number
+        $startNumber = ($page > $jumlahNumber)? $page - $jumlahNumber : 1; 
+        
+        // Untuk akhir link number
+        $endNumber = ($page < ($jumlahPage - $jumlahNumber))? $page + $jumlahNumber : $jumlahPage; 
+        
+        for($i = $startNumber; $i <= $endNumber; $i++){
+          $linkActive = ($page == $i)? ' class="active"' : '';
+
+          if($kolomCari=="" && $kolomKataKunci==""){
+      ?>
+          <li<?php echo $linkActive; ?>><a href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+
+      <?php
+        }else{
+          ?>
+          <li<?php echo $linkActive; ?>><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+          <?php
+        }
+      }
+      ?>
+      
+      <!-- link Next Page -->
+      <?php       
+       if($page == $jumlahPage){ 
+      ?>
+        <li class="disabled"><a href="#">Next</a></li>
+      <?php
+      }
+      else{
+        $linkNext = ($page < $jumlahPage)? $page + 1 : $jumlahPage;
+       if($kolomCari=="" && $kolomKataKunci==""){
+          ?>
+            <li><a href="index.php?page=<?php echo $linkNext; ?>">Next</a></li>
+       <?php     
+          }else{
+        ?> 
+           <li><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $linkNext; ?>">Next</a></li>
+      <?php
+        }
+      }
+      ?>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
